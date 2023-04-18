6ae9e6832f20e728ed092fb735f3723d9f5328b8
MennaHG <105600225+MennaHG@users.noreply.github.com>
2023-04-18 17:15:42 +0200

    Add files via upload

---
+++ prologJava/src/PrologJava.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Map;
+import org.jpl7.*;
+/**
+ * 
+ *
+ * @author menna
+ */
+public class PrologJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        gui.main(null);
+          
+        // TODO code application logic here
+    }
+    
+}
